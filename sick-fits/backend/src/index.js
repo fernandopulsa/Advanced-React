93fd64af7bade693eafc68412c69a7894abd022d
Fernando Pulsa <fernandopulsa@MacBook-Pro-de-Fernando.local>
2019-09-20 17:45:23 +0200

    Added a lot changes on Front and Back

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,17 @@
 // let's go!
+require("dotenv").config({ path: "variables.env" });
+
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+server.start(
+  {
+    credentials: true,
+    origin: process.env.PORT
+  },
+  deets => {
+    console.log("Server is running...", deets);
+  }
+);
